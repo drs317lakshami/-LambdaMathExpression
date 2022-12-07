62733d57d8c724c6336aded83751cebcd487646c
drs317lakshami <devrajkumar317@gmail.com>
2022-12-08 00:52:00 +0530

    Added : Perform math function

---
+++ MathOperation.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.lambda;
+
+@FunctionalInterface
+interface IMathFunction {
+    int calculate(int a, int b);
+}
+
+public class MathOperation {
+
+    public static void main(String[] args) {
+      System.out.println("welcome to lambda expression");
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # -LambdaMathExpression
+# -LambdaMathExpression
