829fe7777ec567f41198bd8fa6104947b75cc9db
Devraj Kumar <109935648+drs317lakshami@users.noreply.github.com>
2022-12-08 01:08:56 +0530

    Added: Perform math function

--- MathOperation.java
+++ MathOperation.java
@@ -8,6 +8,12 @@ interface IMathFunction {
 public class MathOperation {
 
     public static void main(String[] args) {
-      System.out.println("welcome to lambda expression");
+        IMathFunction add = Integer::sum;
+        IMathFunction multiply = (int a, int b) -> a * b;
+        IMathFunction divide = (int a, int b) -> a / b;
+
+        System.out.println("Addition is: " + add.calculate(6, 3));
+        System.out.println("Multiplication is: " + multiply.calculate(6, 3));
+        System.out.println("Division is: " + divide.calculate(6, 3));
     }
 }
